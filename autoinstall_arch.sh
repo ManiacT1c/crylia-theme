c065eca335cc2a981295a3ac71d3c6e9f14b8695
Maniac <106782593+ManiacT1c@users.noreply.github.com>
2022-06-21 23:50:15 +0000

    Update autoinstall_arch.sh

--- autoinstall_arch.sh
+++ autoinstall_arch.sh
@@ -8,12 +8,12 @@ echo "	 / ___ / /_/ / / / / /_/ / /___/ _, _/ / / /____/ // ___ | "
 echo "	/_/  |_\____/ /_/  \____/\____/_/ |_| /_/_____/___/_/  |_| "
 echo " "   
 
-yay -S awesome-git rofi-git picom-jonaburg-git ttf-meslo-nerd-font-powerlevel10k
+paru -S awesome-git rofi-git picom-jonaburg-git ttf-meslo-nerd-font-powerlevel10k
 
 sudo pacman -S papirus-icon-theme pulseaudio-alsa upower bluez bluez-utils xorg-setxkbmap xfce4-power-manager playerctl lightdm light-locker alacritty thunar flameshot
 
 cd
-git clone --recurse-submodules https://github.com/Crylia/crylia-theme
+git clone --recurse-submodules https://github.com/ManiacT1c/crylia-theme
 cd crylia-theme
 cp -r awesome ~/.config/.
 cp -r picom.conf ~/.config/.
